cebb820d910d67889b8fc8558332e47010caa728
josephliao542211 <billan452211@gmail.com>
2026-04-11 20:07:04 -0400

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Jira-meeting-bot-TD-Hackathon
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => res.json({ ok: true }));
+
+const PORT = process.env.PORT ?? 3000;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
